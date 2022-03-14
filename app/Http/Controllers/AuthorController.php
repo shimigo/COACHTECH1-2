a32d565a530e49bc0387fec00738ea9d8ac858cc
shimigo <shimigo1965@yahoo.co.jp>
2022-03-14 17:34:46 +0900

    first-comm

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
+use Illuminate\Support\Facades\Auth;//追記
 
 class AuthorController extends Controller
 {
     public function index()
     {
-        $items = Author::Paginate(4);
-        return view('index', ['items' => $items]);
-    } 
+        $user = Auth::user();
+        $items = Author::paginate(4);
+        $param = ['items' => $items, 'user' =>$user];
+        return view('index', $param);
+    }
     public function find()
     {
         return view('find', ['input' => '']);
@@ -72,5 +75,23 @@ class AuthorController extends Controller
         $noItems = Author::doesntHave('book')->get();
         $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
         return view('author.index',$param);
+    }
+    public function check(Request $request)
+    {
+    $text = ['text' => 'ログインして下さい。'];
+    return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+    $email = $request->email;
+    $password = $request->password;
+    if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+        $text =   Auth::user()->name . 'さんがログインしました';
+    } else {
+        $text = 'ログインに失敗しました';
+    }
+    return view('auth', ['text' => $text]);
     }
 }
